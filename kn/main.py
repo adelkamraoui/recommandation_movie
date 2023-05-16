9dc076e643c24fd6bfc9faf9ee7e0e94446548f2
adelkamraoui <83215937+adelkamraoui@users.noreply.github.com>
2023-05-16 10:24:33 +0100

    Add files via upload

--- kn/main.py
+++ kn/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+from pandas_profiling import ProfileReport
 # Necessary libraries
 import PySimpleGUI as sg
 import numpy as np
@@ -14,16 +15,19 @@ import csv
 import movieposters as mp
 import urllib.request
 from PIL import Image
+import ydata_profiling
 from PIL import Image, ImageTk
 from urllib import request
 import PySimpleGUI as sg
 import os
 from flask import Flask, render_template, request
-from test import recommend_movies,remove_year,suggest_diverse_movies,tuple_list_to_dataframe,dataframe_to_tuple_list,get_movie_title,get_movie_trailer
+from test import get_next_id,recommend_movies,remove_year,suggest_diverse_movies,tuple_list_to_dataframe,dataframe_to_tuple_list,get_movie_title,get_movie_trailer,get_movie_description,get_movie_url_from_csv
 import requests
 import time
 from flask import Flask, render_template, request, session, redirect, url_for
-
+import hashlib
+import matplotlib.pyplot as plt
+from IPython.display import display, HTML
 app = Flask(__name__,template_folder='template')
 app.secret_key = 'secret_key'
 field_names = ['userId', 'movieId', 'rating',
@@ -31,38 +35,40 @@ field_names = ['userId', 'movieId', 'rating',
 
 
 users = {}
-with open('user_credentials.csv') as f:
+with open('output.csv') as f:
        for line in f.readlines():
-        user_id, username, password = line.strip().split(',')
-        users[username] = (user_id, password)
-
+        user_id, username, password,role = line.strip().split(',')
+        users[username] = (user_id, password,role)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    with open('user_credentials.csv') as f:
-            for line in f.readlines():
-                user_id, username, password = line.strip().split(',')
-                users[username] = (user_id, password)
+    with open('output.csv') as f:
+        for line in f.readlines():
+            user_id, username, password, role = line.strip().split(',')
+            users[username] = (user_id, password, role)
 
-        
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if username in users and users[username][1] == password:
+        if username in users and hashlib.sha256(password.encode()).hexdigest() == users[username][1]:
             session['username'] = username
             session['user_id'] = users[username][0]
-            return redirect(url_for('main'))
+            
+            if users[username][2] == 'admin':
+                print('admin salem alikoum')
+                return redirect(url_for('admin'))
+            else:
+                return redirect(url_for('main'))
         else:
             error = 'Invalid username or password'
             return render_template('login.html', error=error)
     else:
         return render_template('login.html')
-
-
+    
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    with open('user_credentials.csv', 'r') as f:
+    with open('output.csv', 'r') as f:
         reader = csv.reader(f)
         headers = next(reader)
         max_id = max(int(row[0]) for row in reader)
@@ -72,9 +78,9 @@ def register():
         user_id = max_id + 1
         username = request.form['username']
         password = request.form['password']
-        
+        role='normal'
         # Check if the username is already taken
-        with open('user_credentials.csv', 'r') as f:
+        with open('output.csv', 'r') as f:
             reader = csv.reader(f)
             headers = next(reader)
             for row in reader:
@@ -83,9 +89,9 @@ def register():
                     return render_template('signin.html', error=error)
         
         # Add the new user to the CSV file
-        with open('user_credentials.csv', 'a', newline='') as f:
+        with open('output.csv', 'a', newline='') as f:
             writer = csv.writer(f)
-            writer.writerow([user_id, username, password])
+            writer.writerow([user_id, username, hashlib.sha256(password.encode()).hexdigest(),role])
         
         # Redirect the user to the login page
         return redirect(url_for('showfilms', id=user_id))
@@ -105,6 +111,12 @@ def main():
         eda_rating = pd.DataFrame(df.groupby('title')['rating'].mean())
         eda_rating['count of ratings'] = pd.DataFrame(df.groupby('title')['rating'].count())
         mtrx_df = rating.pivot(index = 'userId', columns ='movieId', values = 'rating').fillna(0)
+        print('avant')
+        print(mtrx_df.head(30))
+        mtrx_df.head(30)
+        
+
+
         mtrx = mtrx_df.to_numpy()
         ratings_mean = np.mean(mtrx, axis = 1)
         normalized_mtrx = mtrx - ratings_mean.reshape(-1, 1)
@@ -112,6 +124,9 @@ def main():
         sigma = np.diag(sigma)
         all_predicted_ratings = np.dot(np.dot(U, sigma), Vt) + ratings_mean.reshape(-1, 1)
         preds_df = pd.DataFrame(all_predicted_ratings, columns = mtrx_df.columns)
+        print('apres')
+        print(preds_df.head(10))
+        preds_df.head(10)
         already_rated, predictions = recommend_movies(preds_df,userIdd, movie, rating, 10)
         
         print(already_rated.head(10))
@@ -122,19 +137,8 @@ def main():
         api_key = 'ccfc2af2a0cd4597bf0472fab1af2f02'  # Replace with your actual TMDb API key
 
         for prediction in predictions['title']:
-            pre=remove_year(prediction)
-            url = f'https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={pre}'
-            response = requests.get(url)
-            data = response.json()
-            if data['total_results'] > 0:
-                poster_path = data['results'][0]['poster_path']
-                poster_url = f'https://image.tmdb.org/t/p/w500/{poster_path}'
-                print("+1")
-                poster_urls.append(poster_url)
-            else:
-                poster_url = 'https://i.quotev.com/b2gtjqawaaaa.jpg'  # Replace with your actual error image path
-                poster_urls.append(poster_url)    
-                print("+1 taa errror")
+            poster_url=get_movie_url_from_csv('/home/adel/Desktop/kn/movieswithurl.csv', prediction)
+            poster_urls.append(poster_url)
 
         predictions['urls']=poster_urls        
         print(predictions)
@@ -151,6 +155,88 @@ def main():
 def enteridrecom():
     return render_template('recommand.html')
 
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/Factorisation')
+def Factorisation():
+    rating = pd.read_csv('/home/adel/Desktop/kn/ratings.csv')
+    movie = pd.read_csv('/home/adel/Desktop/kn/movies.csv')
+    df = pd.merge(rating, movie, on='movieId')
+    eda_rating = pd.DataFrame(df.groupby('title')['rating'].mean())
+    eda_rating['count of ratings'] = pd.DataFrame(df.groupby('title')['rating'].count())
+    mtrx_df = rating.pivot(index='userId', columns='movieId', values='rating').fillna(0)
+
+    page = request.args.get('page', default=1, type=int)
+    chunk_size = 10  # Number of rows per page
+    start_idx = (page - 1) * chunk_size
+    end_idx = start_idx + chunk_size
+
+    mtrx_df_page = mtrx_df.iloc[start_idx:end_idx]
+    mtrx_df_html = mtrx_df_page.to_html()
+
+    mtrx = mtrx_df.to_numpy()
+    ratings_mean = np.mean(mtrx, axis=1)
+    normalized_mtrx = mtrx - ratings_mean.reshape(-1, 1)
+    U, sigma, Vt = svds(normalized_mtrx, k=50)
+    sigma = np.diag(sigma)
+    all_predicted_ratings = np.dot(np.dot(U, sigma), Vt) + ratings_mean.reshape(-1, 1)
+    preds_df = pd.DataFrame(all_predicted_ratings, columns=mtrx_df.columns)
+
+    preds_df_page = preds_df.iloc[start_idx:end_idx]
+    preds_df_html = preds_df_page.to_html()
+
+    num_pages = int(np.ceil(len(mtrx_df) / chunk_size))
+
+    return render_template('factorisation_en_temps_reel.html', mtrx_df_html=mtrx_df_html, preds_df_html=preds_df_html, page=page, num_pages=num_pages)
+
+
+@app.route('/ajoutfilm', methods=['GET', 'POST'])
+def ajoutfilm():
+    
+    if request.method == 'POST':
+        # Récupérer les données du formulaire
+        title = request.form['title']
+        genres = request.form['genres']
+
+        # Générer l'ID du film
+        movie_id = get_next_id()
+        print('je suis le maxxxxxx')
+        print(movie_id)
+        # Ajouter le nouveau film au fichier CSV
+        CSV_FILE = '/home/adel/Desktop/kn/movies.csv'
+        with open(CSV_FILE, 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([movie_id, title, genres])
+    
+        return 'Film ajouté avec succès !'
+    
+    
+    return render_template('ajoutfilm.html')
+@app.route('/supprimefilm', methods=['GET', 'POST'])
+def supprimefilm():
+    if request.method == 'POST':
+        # Get the film name from the form
+        film_name = request.form['film_name']
+
+        # Delete the film from the CSV file
+        CSV_FILE = '/home/adel/Desktop/kn/movies.csv'
+        updated_rows = []
+        with open(CSV_FILE, 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if row[1] != film_name:  # Exclude the film with the specified name
+                    updated_rows.append(row)
+
+        with open(CSV_FILE, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(updated_rows)
+
+        return 'Film deleted successfully!'
+
+    return render_template('supprimefilm.html')
 @app.route('/allfilms/<int:id>')
 def showfilms(id):
     movie = pd.read_csv('/home/adel/Desktop/kn/movieswithurl.csv')
@@ -206,7 +292,8 @@ def ratingtemp(id,movieId):
     movieidd=movieId
     urll=get_movie_trailer('ccfc2af2a0cd4597bf0472fab1af2f02',pre)
     print(movieidd)
-    return render_template('ratingg.html', idd=idd,movieidd=movieidd,title=title,poster_url=poster_url,urll=urll)
+    description=get_movie_description(pre,'ccfc2af2a0cd4597bf0472fab1af2f02')
+    return render_template('ratingg.html', idd=idd,movieidd=movieidd,title=title,poster_url=poster_url,urll=urll,description=description)
 @app.route('/allfilms/<int:id>/<int:movieId>/commit', methods=['POST'])
 def ratee(id,movieId):
     rating = request.form['rating']
@@ -312,8 +399,13 @@ def diversification(idd):
     sigma = np.diag(sigma)
     all_predicted_ratings = np.dot(np.dot(U, sigma), Vt) + ratings_mean.reshape(-1, 1)
     preds_df = pd.DataFrame(all_predicted_ratings, columns = mtrx_df.columns)
-    already_rated, predictions = recommend_movies(preds_df,idd, movie, rating, 20)
-    recom=dataframe_to_tuple_list(predictions)
+    '''
+    already_rated, li1 = recommend_movies(preds_df, idd, movie, rating, 20)
+    pertinences = []
+
+    
+
+    recom = dataframe_to_tuple_list(li1)
 
     # Calcul du score de diversité pour chaque film
     diversity_scores = []
@@ -323,7 +415,51 @@ def diversification(idd):
         for j in range(len(recom)):
             if i != j:
                 genres_j = set(recom[j][2].split("|"))
-                distance = len(genres_i.symmetric_difference(genres_j))
+                distance = 1 - (len(genres_i.intersection(genres_j)) / len(genres_i.union(genres_j)))
+                diversity_sum += distance
+            diversity_score = diversity_sum / (len(recom) - 1)
+            diversity_scores.append((recom[i][0], recom[i][1], recom[i][2], diversity_score))
+
+        # Trier les films par ordre décroissant de score de diversité et afficher les 10 premiers
+        sorted_diversity_scores = sorted(diversity_scores, key=lambda x: x[3], reverse=True)
+        diversified = tuple_list_to_dataframe(sorted_diversity_scores)
+        diversified = diversified.head(10)
+        
+        
+        # Calculer le nombre de films communs entre la liste initiale et la liste diversifiée
+        films_communs = set(li1['title']).intersection(set(diversified10['title']))
+
+        nb_films_communs = len(films_communs)
+
+        # Calculer le pourcentage de pertinence
+        pourcentage_pertinence = nb_films_communs / len(li1) * 100
+        # Afficher le résultat
+        print("La pertinence de la liste diversifiée avec {} films est de : {:.2f}%".format(n, pourcentage_pertinence))
+
+        # Ajouter la pertinence à la liste
+        pertinences.append(pourcentage_pertinence)
+    
+    print(pertinences)
+    plt.plot(range(10, 51), pertinences)
+    plt.xlabel('Nombre de films recommandés')
+    plt.ylabel('Pertinence (%)')
+    plt.title('Évolution de la pertinence en fonction du nombre de films recommandés')
+    plt.show()    
+      '''                                                                                                                      
+    already_rated, list1 = recommend_movies(preds_df,idd, movie, rating, 10)
+   
+    already_rated, predictions = recommend_movies(preds_df,idd, movie, rating, 20)
+    recom=dataframe_to_tuple_list(predictions)
+    
+    # Calcul du score de diversité pour chaque film
+    diversity_scores = []
+    for i in range(len(recom)):
+        genres_i = set(recom[i][2].split("|"))
+        diversity_sum = 0
+        for j in range(len(recom)):
+            if i != j:
+                genres_j = set(recom[j][2].split("|"))
+                distance = 1 - (len(genres_i.intersection(genres_j)) / len(genres_i.union(genres_j)))
                 diversity_sum += distance
         diversity_score = diversity_sum / (len(recom) - 1)
         diversity_scores.append((recom[i][0], recom[i][1], recom[i][2], diversity_score))
@@ -332,27 +468,29 @@ def diversification(idd):
     print('ici')
     
     sorted_diversity_scores = sorted(diversity_scores, key=lambda x: x[3], reverse=True)
-   
+    
+    
     print(tuple_list_to_dataframe(sorted_diversity_scores))
     diversified=tuple_list_to_dataframe(sorted_diversity_scores)
-  
+    '''
+    print('look adel i made it')
+    reco_initiale=list1
+    reco_diversifiee=diversified
+    films_communs = set(reco_initiale).intersection(set(reco_diversifiee))
+    nb_films_communs = len(films_communs)
+
+    # Calculez le pourcentage de pertinence
+    pourcentage_pertinence = nb_films_communs / len(reco_initiale) * 100
+
+    print("La pertinence de la liste diversifiée est de : {:.2f}%".format(pourcentage_pertinence))
+    '''
     poster_urls = []
     api_key = 'ccfc2af2a0cd4597bf0472fab1af2f02'  # Replace with your actual TMDb API key
-
+    diversified = diversified.head(10)
     for diversifiedd in diversified['title']:
-        pre=remove_year(diversifiedd)
-        url = f'https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={pre}'
-        response = requests.get(url)
-        data = response.json()
-        if data['total_results'] > 0:
-            poster_path = data['results'][0]['poster_path']
-            poster_url = f'https://image.tmdb.org/t/p/w500/{poster_path}'
-            print("+1")
+            poster_url=get_movie_url_from_csv('/home/adel/Desktop/kn/movieswithurl.csv', diversifiedd)
             poster_urls.append(poster_url)
-        else:
-            poster_url = 'https://i.quotev.com/b2gtjqawaaaa.jpg'  # Replace with your actual error image path
-            poster_urls.append(poster_url)    
-            print("+1 taa errror")
+    
 
     diversified['urls']=poster_urls        
     
--- kn/test.py
+++ kn/test.py
@@ -49,6 +49,7 @@ def recommend_movies(preds_df, userId, movie, ratings_df, num_recommendations=5)
     user_row_number = userId-1 
     # Sort user's predictons
     sorted_user_predictions = preds_df.iloc[user_row_number].sort_values(ascending=False) 
+    print(sorted_user_predictions.head(10))
     # List movies user already rated
     user_data = ratings_df[ratings_df.userId == (userId)]
     user_rated = (user_data.merge(movie, how = 'left', left_on = 'movieId', right_on = 'movieId').
@@ -252,7 +253,7 @@ def dataframe_to_tuple_list(df):
     return tuple_list
 
 recom=recommendationss
-
+print(recom)
 # Calcul du score de diversité pour chaque film
 diversity_scores = []
 for i in range(len(recom)):
@@ -261,7 +262,13 @@ for i in range(len(recom)):
     for j in range(len(recom)):
         if i != j:
             genres_j = set(recom[j][2].split("|"))
-            distance = len(genres_i.symmetric_difference(genres_j))
+            #print(len(genres_i.intersection(genres_j)))
+            #print('/')
+            #print(len(genres_i.union(genres_j)))
+            distance = 1 - (len(genres_i.intersection(genres_j)) / len(genres_i.union(genres_j)))
+            #print(distance)
+            #print(i)
+            #distance = len(genres_i.symmetric_difference(genres_j))
             diversity_sum += distance
     diversity_score = diversity_sum / (len(recom) - 1)
     diversity_scores.append((recom[i][0], recom[i][1], recom[i][2], diversity_score))
@@ -272,7 +279,7 @@ sorted_diversity_scores = sorted(diversity_scores, key=lambda x: x[3], reverse=T
 for i in range(10):
     print(sorted_diversity_scores[i][1], sorted_diversity_scores[i][3])
 
-print(tuple_list_to_dataframe(sorted_diversity_scores))
+print(sorted_diversity_scores)
 
 
 def get_movie_title(movie_id):
@@ -306,6 +313,7 @@ def get_movie_trailer(api_key, movie_title):
     # Send the API request and parse the response
     response = requests.get(movie_url)
     data = json.loads(response.text)
+    print(data)
 
     # Extract the trailer key for the first video result
     trailer_key = data["videos"]["results"][0]["key"]
@@ -315,3 +323,169 @@ def get_movie_trailer(api_key, movie_title):
    
     # Return the movie title and trailer URL as a tuple
     return (trailer_url)
+import requests
+
+def get_movie_description(movie_name, api_key):
+    # Construct the URL for the TMDB API search endpoint
+    url = f'https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={movie_name}'
+
+    # Make a GET request to the API
+    response = requests.get(url)
+
+    # Get the JSON data from the response
+    data = response.json()
+    print(data)
+    # Check if any results were found
+    if 'total_results' not in data or data['total_results'] == 0:
+        return 'No results found for the given movie name.'
+    
+    # Get the ID of the first movie in the search results
+    movie_id = data['results'][0]['id']
+    
+    # Construct the URL for the TMDB API movie detail endpoint
+    url = f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}'
+
+    # Make another GET request to the API
+    response = requests.get(url)
+
+    # Get the JSON data from the response
+    data = response.json()
+
+    # Get the movie description from the data
+    description = data['overview']
+
+    return description
+
+def get_movie_url_from_csv(csv_file, movie_title):
+    with open(csv_file, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)  # skip header row
+        for row in reader:
+            if row[1] == movie_title:
+                return row[3]
+    return None  # return None if movie not found
+"""
+import numpy as np
+import pandas as pd
+from scipy.sparse.linalg import svds
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+import matplotlib.pyplot as plt
+
+# Load the data
+rating = pd.read_csv('/home/adel/Desktop/kn/ratings.csv')
+movie = pd.read_csv('/home/adel/Desktop/kn/movies.csv')
+df = pd.merge(rating, movie, on='movieId')
+
+# Calculate the mean rating for each movie
+mean_rating = pd.DataFrame(df.groupby('title')['rating'].mean())
+
+# Calculate the number of ratings for each movie
+mean_rating['count of ratings'] = pd.DataFrame(df.groupby('title')['rating'].count())
+
+# Create a pivot table of the ratings data
+mtrx_df = rating.pivot(index = 'userId', columns ='movieId', values = 'rating').fillna(0)
+mtrx = mtrx_df.to_numpy()
+
+# Calculate the mean rating for each user
+ratings_mean = np.mean(mtrx, axis = 1)
+
+# Normalize the ratings matrix by subtracting the mean rating for each user
+normalized_mtrx = mtrx - ratings_mean.reshape(-1, 1)
+
+# Calculate the SVD decomposition of the normalized ratings matrix
+rmse_values = []
+k_values = range(1, 51)
+for k in k_values:
+    U, sigma, Vt = svds(normalized_mtrx, k = k)
+    sigma = np.diag(sigma)
+
+    # Calculate the predicted ratings
+    all_predicted_ratings = np.dot(np.dot(U, sigma), Vt) + ratings_mean.reshape(-1, 1)
+
+    # Extract the actual ratings from the training data
+    actual_ratings = mtrx
+
+    # Calculate the RMSE
+    rmse = sqrt(mean_squared_error(actual_ratings, all_predicted_ratings))
+    rmse_values.append(rmse)
+
+# Plot the RMSE values for different values of K
+plt.plot(k_values, rmse_values, 'bx-')
+plt.xlabel('Number of latent features (K)')
+plt.ylabel('RMSE')
+plt.title('RMSE vs. Number of Latent Features')
+plt.show()
+"""
+"""
+def get_user_movie_rating(userId, movieId, ratings_df):
+    user_movie_rating = ratings_df[(ratings_df['userId'] == userId) & (ratings_df['movieId'] == movieId)]['rating'].values
+    if len(user_movie_rating) > 0:
+        return user_movie_rating[0]
+    else:
+        return "Aucune note trouvée pour cet utilisateur et ce film."
+
+# Utilisation de la fonction pour obtenir la note donnée par un utilisateur pour un film
+userId = 1
+movieId = 3  # Remplacez par l'ID du film souhaité
+ratings_df=pd.read_csv('/home/adel/Desktop/kn/ratings.csv')
+rating = get_user_movie_rating(userId, movieId, ratings_df)
+print(f"La note donnée par l'utilisateur {userId} pour le film avec l'ID {movieId} est : {rating}")
+"""
+"""
+def get_user_movie_rating(userId, movie_df, ratings_df):
+    user_movie_ratings = []
+    for index, row in movie_df.iterrows():
+        movie_id = row['movieId']
+        ratings = ratings_df[(ratings_df['userId'] == userId) & (ratings_df['movieId'] == movie_id)]['rating'].values
+        if len(ratings) > 0:
+            user_movie_ratings.append(ratings[0])
+        else:
+            user_movie_ratings.append(None)
+    return user_movie_ratings
+
+
+# Usage: Get ratings for a DataFrame of movies for a specific user
+userId = 616
+movie_df = pd.read_csv('/home/adel/Desktop/kn/movies.csv')  # Replace with the actual path to your movie DataFrame
+ratings_df = pd.read_csv('/home/adel/Desktop/kn/ratings.csv')  # Replace with the actual path to your ratings DataFrame
+ratings = get_user_movie_rating(userId, movie_df, ratings_df)
+print(f"Ratings given by user {userId} for the movies:")
+print(ratings)
+"""
+
+"""
+# Path to the input CSV file
+input_file = '/home/adel/Desktop/kn/user_credentials.csv'
+
+# Path to the output CSV file
+output_file = 'output.csv'
+
+# Open the input file in read mode
+with open(input_file, 'r') as file:
+    # Read the CSV data
+    reader = csv.reader(file)
+    rows = list(reader)
+
+# Add "role" column header and update values
+for row in rows:
+    row.append("role")
+    row[3] = "normal"
+
+# Open the output file in write mode
+with open(output_file, 'w', newline='') as file:
+    # Write the updated data to the output CSV file
+    writer = csv.writer(file)
+    writer.writerows(rows)
+
+print("CSV file processing completed. Output file: output.csv")
+"""
+CSV_FILE = '/home/adel/Desktop/kn/movies.csv'
+
+def get_next_id():
+    # Lecture du fichier CSV et récupération de la valeur maximale de movieId
+    with open(CSV_FILE, 'r') as file:
+        reader = csv.DictReader(file)
+        movie_ids = [int(row['movieId']) for row in reader]
+    next_id = max(movie_ids) + 1 if movie_ids else 1
+    return str(next_id)
